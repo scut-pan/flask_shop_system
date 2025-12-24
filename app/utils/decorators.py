df37be96297dd71d57d5330134d6a5d469e43017
scut-pan <984582466@qq.com>
2025-12-24 12:25:44 +0800

    feat: 实现管理员权限装饰器和游客权限装饰器

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -1,1 +1,24 @@
 """装饰器"""
+from functools import wraps
+from flask import abort, flash, redirect, url_for
+from flask_login import current_user
+
+def admin_required(f):
+    """管理员权限装饰器"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not current_user.is_authenticated or not current_user.is_admin:
+            flash('需要管理员权限访问此页面', 'danger')
+            return redirect(url_for('main.index'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def anonymous_required(f):
+    """游客（未登录）权限装饰器"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated:
+            flash('您已经登录', 'info')
+            return redirect(url_for('main.index'))
+        return f(*args, **kwargs)
+    return decorated_function
