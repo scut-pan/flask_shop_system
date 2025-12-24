0a4d5ea80b7b68ad67b80cf6b369b7b42b6f9273
scut-pan <984582466@qq.com>
2025-12-24 12:12:17 +0800

    feat: 更新模型初始化文件

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,7 @@
-# 数据模型
+"""数据模型"""
+from .user import User
+from .product import Product
+from .order import Order, OrderItem
+from .cart import CartItem
+
+__all__ = ['User', 'Product', 'Order', 'OrderItem', 'CartItem']
