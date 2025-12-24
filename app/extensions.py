4ea3b4b17fb12ce599a43e2753f814d0edaf6025
scut-pan <984582466@qq.com>
2025-12-24 11:51:59 +0800

    feat: 编辑Flask扩展配置文件

--- app/extensions.py
+++ app/extensions.py
@@ -1,1 +1,23 @@
-# Flask 扩展初始化
+"""Flask 扩展初始化"""
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_migrate import Migrate
+
+# 初始化扩展
+db = SQLAlchemy()
+login_manager = LoginManager()
+mail = Mail()
+migrate = Migrate()
+
+def init_extensions(app):
+    """初始化所有扩展"""
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    migrate.init_app(app, db)
+
+    # 配置登录管理器
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = '请先登录访问此页面'
+    login_manager.login_message_category = 'info'
