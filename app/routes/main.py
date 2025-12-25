d0ec6653bee3ff0a0b7786cd6584a14410284359
scut-pan <984582466@qq.com>
2025-12-25 13:40:50 +0800

    feat: 实现主页路由

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,1 +1,14 @@
 """主页路由"""
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    """网站首页"""
+    return render_template('main/index.html')
+
+@main_bp.route('/about')
+def about():
+    """关于我们页面"""
+    return render_template('main/about.html')
